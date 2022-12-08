e4581bd2d67e229226ffbebb985b03f341acefec
Pavel Mikhaylenko <104693653+westteern@users.noreply.github.com>
2022-12-08 18:33:14 +0200

    fatch image work

---
+++ src/api/fatchImages.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '30721488-ec9ca19b7cca22464bcdf3786';
+
+export default async function fetchImage(userInput, pageNumber) {
+  try {
+    const responce = await axios.get(BASE_URL, {
+      params: {
+        key: KEY,
+        q: userInput,
+        imageType: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        page: `${pageNumber}`,
+        per_page: '12',
+      },
+    });
+    //   return await responce.data;
+      console.log(responce.data);
+  } catch (error) {
+      console.log(error);
+  }
+}
+fetchImage()
+
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { GlobalStyle } from './GlobalStyle';
 export const App = () => {
   return (
     <>
+      <p>Hello world!</p>
       <GlobalStyle />
     </>
   );
